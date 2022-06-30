7f4efaa5776095ed66a64b3f6613aba8ee4fb342
thescientist230803 <thescienistian@gmail.com>
2022-06-30 16:17:02 -0700

    first printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ * printf- prints outcoe is a piece of art
+ * return - return 0 if exited properlynon zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
