d37ae51bb98e500d428363f93382002372d6f6b3
ianinco <58395493+thescientist230803@users.noreply.github.com>
2022-07-02 02:36:47 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,20 @@
-7-print_tebahpla.c
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+  
+}
