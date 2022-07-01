5c47e5640d59504eff323bfe59df8a194911ab23
ianinco <58395493+thescientist230803@users.noreply.github.com>
2022-07-02 02:43:49 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,21 @@
-8-print_base16.c
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (success/correct)
+ */
+int main(void)
+{
+	char base_16;
+  
+  for (base_16 = 48; base_16 <= 57; base_16++)
+{
+putchar(base_16);
+}
+  for (base_16 = 'a'; base_16 <= 'f'; base_16++)
+	{
+		putchar(base_16);
+	}
+  putchar('\n');
+	return (0);
+}
