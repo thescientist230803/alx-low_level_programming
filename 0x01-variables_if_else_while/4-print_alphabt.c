3d1e88b1e98cd1570a8cc6d517db7ac1da4d9b06
ianinco <58395493+thescientist230803@users.noreply.github.com>
2022-07-02 02:20:18 +0300

    done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,23 @@
-4-print_alphabt.c
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+		{
+			putchar(alphabet);
+		}
+		alphabet++;
+	}
+  
+	putchar('\n');
+  
+	return (0);
+}
