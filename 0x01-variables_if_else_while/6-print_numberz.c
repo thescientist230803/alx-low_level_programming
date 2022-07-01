3b23cd188df57ba98c505066912f1be471045a0c
ianinco <58395493+thescientist230803@users.noreply.github.com>
2022-07-02 02:32:04 +0300

    5repeat

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,21 @@
-6-print_numberz.c
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (success/correect)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 0 ; i < 10; i++)
+{
+printf("%i", i);
+}
+
+printf("\n");
+
+return (0);
+
+}
