642c880ad7bb89fb6fc2ce0479de5f7fef50c80b
ianinco <58395493+thescientist230803@users.noreply.github.com>
2022-07-02 02:45:01 +0300

    update to base_16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,19 @@
  */
 int main(void)
 {
-	char base_16;
+char base_16;
   
-  for (base_16 = 48; base_16 <= 57; base_16++)
+for (base_16 = 48; base_16 <= 57; base_16++)
 {
 putchar(base_16);
 }
-  for (base_16 = 'a'; base_16 <= 'f'; base_16++)
-	{
-		putchar(base_16);
-	}
-  putchar('\n');
-	return (0);
+for (base_16 = 'a'; base_16 <= 'f'; base_16++)
+{
+putchar(base_16);
+}
+	
+putchar('\n');
+	
+return (0);
+	
 }
