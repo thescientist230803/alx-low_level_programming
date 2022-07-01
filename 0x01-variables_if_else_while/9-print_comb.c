94f4386263c7feec290eafaa1387bfcb7b9ca203
ianinco <58395493+thescientist230803@users.noreply.github.com>
2022-07-02 02:51:45 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,22 @@
-9-print_comb.c
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 48; number < 58; number++)
+	{
+		putchar(number);
+		if (number != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
