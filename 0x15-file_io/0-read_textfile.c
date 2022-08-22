af69825f3a6220ccd185faddec27653c57c043ab
ianinco <58395493+thescientist230803@users.noreply.github.com>
2022-08-23 01:58:02 +0300

    edit on 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,6 +3,7 @@
 /**
  * read_textfile - reads a text file and prints it to the standard output
  * @filename: name of the file to be read
+ * @letters: number of letters to read and print
  *
  * Return: the number of letters printed, or 0 if it failed
  */
